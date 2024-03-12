258e2672295837c7d969377d9b296445b63bfa52
anasyakubu <yakubuanas04@gmail.com>
2024-03-12 17:56:47 +0100

    Fixed Bug

--- app/utils/cn.js
+++ app/utils/cn.js
@@ -1,6 +1,20 @@
-import { ClassValue, clsx } from "clsx";
+import React from "react";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export default function YourComponent() {
+  const classes = cn("class1", "class2");
+
+  return <div className={classes}></div>;
+}
+
+// import { ClassValue, clsx } from "clsx";
+// import { twMerge } from "tailwind-merge";
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
+// }
